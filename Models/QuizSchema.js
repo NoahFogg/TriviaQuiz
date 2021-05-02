9164de7e9de19ec47099ec08b0304e26da062ae4
fogg.noah32 <fogg.noah32@gmail.com>
2021-05-02 14:24:47 -0400

    Update

--- Models/QuizSchema.js
+++ Models/QuizSchema.js
@@ -1,2 +1,20 @@
 let mongoose = require('mongoose');
 let dbManager = require ('./dbManager');
+
+var quizSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: 'Field is required'
+    },
+    email: {
+        type: String,
+        required: 'This is required too.'
+    },
+    mobile: {
+        type: Number,
+        required: 'This is also required'
+    }
+
+})
+
+mongoose.model('Student', studentSchema);
